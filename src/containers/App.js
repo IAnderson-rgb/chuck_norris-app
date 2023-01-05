b146f93f62acccdac167490e1dabba60f1514acf
Ianderson-rgb <afamilyenterprises@gmail.com>
2023-01-05 17:29:18 -0500

    adding scroll component

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,49 +7,51 @@ import './App.css';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      jokes: [],
-      searchfield: ""
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			jokes: [],
+			searchfield: '',
+		};
+	}
 
-  async componentDidMount() {
-    const urls = [
-      "https://api.chucknorris.io/jokes/random",
-      "https://api.chucknorris.io/jokes/categories",
-      "https://api.chucknorris.io/jokes/search?query=test"
-    ];
+	async componentDidMount() {
+		const urls = [
+			'https://api.chucknorris.io/jokes/random',
+			'https://api.chucknorris.io/jokes/categories',
+			'https://api.chucknorris.io/jokes/search?query=test',
+		];
 
-    const getData = async function () {
-      const data = await Promise.allSettled(
-        urls.map(async function (url) {
-          const response = await fetch(url);
-          const resp = await response.json();
-          return resp;
-        })
-      );
+		const getData = async function () {
+			const data = await Promise.allSettled(
+				urls.map(async function (url) {
+					const response = await fetch(url);
+					const resp = await response.json();
+					return resp;
+				})
+			);
 
-      return data.map(({ value }) => value);
-    };
+			return data.map(({ value }) => value);
+		};
 
-    this.setState({ jokes: await getData() });
-  }
+		this.setState({ jokes: await getData() });
+	}
 
-  componentDidUpdate() {}
+	componentDidUpdate() {}
 
-  render() {
-    const { jokes, searchfield } = this.state;
-    console.log("Jokes Here", jokes);
-    return !jokes.length ? (
-      <h1>..Loading</h1>
-    ) : (
-      <div className="tc">
-        <JokePanel />
-      </div>
-    );
-  }
+	render() {
+		const { jokes, searchfield } = this.state;
+		return !jokes.length ? (
+			<h1>..Loading</h1>
+		) : (
+			<div className='tc'>
+				<Scroll>
+					<JokePanel />
+					<SearchBox searchChange={this.onSearchChange} />
+				</Scroll>
+			</div>
+		);
+	}
 }
 
 export default App;
