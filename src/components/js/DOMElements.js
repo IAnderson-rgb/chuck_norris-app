589dd713e3733fb53ba134da1f646c3c07c472bc
Ianderson-rgb <afamilyenterprises@gmail.com>
2023-01-07 04:46:57 -0500

    cleaning up code

--- src/components/js/DOMElements.js
+++
@@ -1,9 +0,0 @@
-const newQuoteBtn = document.getElementById("new-quote")
-const twitterBtn = document.getElementById("twitter")
-const quoteText = document.getElementById("quote")
-const authorText = document.getElementById("author")
-const quoteContainer = document.getElementById("quote-container")
-const loader = document.getElementById("loader")
-
-
-export { newQuoteBtn, twitterBtn, quoteText, authorText, quoteContainer, loader}
--- src/components/js/tweetQuote.js
+++
@@ -1,6 +0,0 @@
-import { quoteText, authorText } from "./DOMElements.js"
-
-export default function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
-  window.open(twitterUrl, "_blank")
-}
